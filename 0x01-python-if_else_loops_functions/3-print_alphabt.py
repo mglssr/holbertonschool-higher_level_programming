bd890d7f6891e25f90398e21183667328269fd2e
mglssr <iglesiasmaia6@gmail.com>
2022-04-26 08:57:37 -0700

    changed conditional

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == 101 or i == 113:
-        i = 0
-    print("{:s}".format(chr(i)), end="")
+    if i != 101 and i != 113:
+        print("{:s}".format(chr(i)), end="")
