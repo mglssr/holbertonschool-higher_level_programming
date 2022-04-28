fb9c58d39a09f7a07413f6599476fefd59666755
mglssr <iglesiasmaia6@gmail.com>
2022-04-28 08:04:06 -0700

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+import add_0 as add
+
+argc = len(sys.argv)
+aux = 0
+
+if argc == 2:
+    aux = int(sys.argv[1])
+elif argc <= 1:
+    aux = 0
+else:
+    for i in range (1, argc):
+        aux += int(sys.argv[i])
+print(f"{aux}")
