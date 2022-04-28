4cf402ee69119c703110f28df4690980ee06ec09
mglssr <iglesiasmaia6@gmail.com>
2022-04-28 06:42:51 -0700

    changed the name of the argument var count

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
-argv = len(sys.argv)
-if argv == 2:
+argc = len(sys.argv)
+if argc == 2:
     print(f"1 argument:")
     print(f"{1}: {sys.argv[1]}")
-elif argv <= 1:
+elif argc <= 1:
     print(f"0 arguments.")
 else:
-    print(f"{argv - 1} arguments:")
-    for i in range(1, argv):
+    print(f"{argc - 1} arguments:")
+    for i in range(1, argc):
         print(f"{i}: {sys.argv[i]}")
