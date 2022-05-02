66bace5e157076186c42480865b7c3fd9f514699
mglssr <iglesiasmaia6@gmail.com>
2022-05-02 13:30:14 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if not tuple_a and tuple_b:
-        return (tuple_b)
-    elif tuple_a and not tuple_b:
+    if tuple_a and not tuple_b:
         return tuple_a
+    elif tuple_b and not tuple_a:
+        return tuple_b
     elif not tuple_a and not tuple_b:
         tuple_3 = (0, 0)
         return tuple_3
     else:
         if len(tuple_a) < 2 and len(tuple_b) < 2:
-            tuple_3 = (tuple_a[0] + tuple_b[0], )
+            tuple_3 = (tuple_a[0] + tuple_b[0], 0)
             return tuple_3
         elif len(tuple_a) < 2 and len(tuple_b) >= 2:
             tuple_3 = (tuple_a[0] + tuple_b[0], tulpe_b[1])
