e285ec9ddfb7f315d1690a6ac50e480da091fe3b
mglssr <iglesiasmaia6@gmail.com>
2022-05-02 13:26:03 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,15 +7,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif not tuple_a and not tuple_b:
         tuple_3 = (0, 0)
         return tuple_3
-    elif len(tuple_a) < 2 and len(tuple_b) < 2:
-        tuple_3 = (tuple_a[0] + tuple_b[0], )
-        return tuple_3
-    elif len(tuple_a) < 2 and len(tuple_b) >= 2:
-        tuple_3 = (tuple_a[0] + tuple_b[0], tulpe_b[1])
-        return tuple_3
-    elif len(tuple_a) >= 2 and len(tuple_b) < 2:
-        tuple_3 = (tuple_a[0] + tuple_b[0], tuple_a[1])
-        return tuple_3
     else:
-        tuple_3 = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return tuple_3
+        if len(tuple_a) < 2 and len(tuple_b) < 2:
+            tuple_3 = (tuple_a[0] + tuple_b[0], )
+            return tuple_3
+        elif len(tuple_a) < 2 and len(tuple_b) >= 2:
+            tuple_3 = (tuple_a[0] + tuple_b[0], tulpe_b[1])
+            return tuple_3
+        elif len(tuple_a) >= 2 and len(tuple_b) < 2:
+            tuple_3 = (tuple_a[0] + tuple_b[0], tuple_a[1])
+            return tuple_3
+        else:
+            tuple_3 = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+            return tuple_3
