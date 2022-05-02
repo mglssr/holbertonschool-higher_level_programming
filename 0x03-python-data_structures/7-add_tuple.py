4d39f4a52c73a566a38a3ac71d2de919a1895b23
mglssr <iglesiasmaia6@gmail.com>
2022-05-02 13:58:54 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-   if tuple_a and not tuple_b:
-        return tuple_a
-    elif tuple_b and not tuple_a:
-        return tuple_b
-    elif not tuple_a and not tuple_b:
-        tuple_3 = (0, 0)
-        return tuple_3
-    else:
-        if len(tuple_a) < 2 and len(tuple_b) < 2:
-            tuple_3 = (tuple_a[0] + tuple_b[0], 0)
-            return tuple_3
-        if len(tuple_a) < 2 and len(tuple_b) >= 2:
-            tuple_3 = (tuple_a[0] + tuple_b[0], tulpe_b[1])
-            return tuple_3
-        if len(tuple_a) >= 2 and len(tuple_b) < 2:
-            tuple_3 = (tuple_a[0] + tuple_b[0], tuple_a[1])
-            return tuple_3
-        if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-            tuple_3 = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-            return tuple_3 
+    len1 = len(tuple_a)
+    len2 = len(tuple_b)
+
+    while len1 < 2:
+       tuple_a = tuple_a + (0,)
+       len1 += 1
+    while len2 < 2:
+        tuple_b = tuple_b + (0,)
+        len2 += 1
+
+    tuple_3 = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return(tuple_3)
