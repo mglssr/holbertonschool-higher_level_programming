a1692b1020bc7579f5f6136f1ec1534a34c3a32b
mglssr <iglesiasmaia6@gmail.com>
2022-05-04 08:13:12 -0700

    remove first if

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        result = 0
-        aux = set(my_list)
-        uniq = list(aux)
-        for i in range (len(uniq)):
-            result += int(uniq[i])
-        return result
+    result = 0
+    aux = set(my_list)
+    uniq = list(aux)
+    for i in range (len(uniq)):
+        result += int(uniq[i])
+    return result
