a5f3d18e24000550a7dd3642ba57b1dd6db677ca
mglssr <iglesiasmaia6@gmail.com>
2022-05-16 12:38:21 -0700

    added anotthe except case

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
             i += 1
         except (TypeError, ValueError):
             i += 1
+        else IndexError:
+            break
     print("")
     return count
