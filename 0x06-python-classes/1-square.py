c2b6fc64fb2c351e1baec3ad414524295cc53572
mglssr <iglesiasmaia6@gmail.com>
2022-05-17 13:57:12 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" function that creates a class 'Square' that defines a square"""
+
+
+class Square:
+    """ define the class 'Square'"""
+    def __init__(self, size=0):
+        """ __init__ method runs as soon as an objet of a class is created.
+        The method is useful to do any initialization (or passing initial
+        values to your object) you want to do with your object."""
+        self.size = size
