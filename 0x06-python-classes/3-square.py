19f2375815ecaefe0fe6402ac3286ea1fd6a0229
mglssr <iglesiasmaia6@gmail.com>
2022-05-17 15:28:58 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" function that creates a class 'Square'"""
+
+
+class Square:
+    """creates the class"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
