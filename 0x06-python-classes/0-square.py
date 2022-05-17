e7e98a212f03819391c1dd26fc45ff1729df869b
mglssr <iglesiasmaia6@gmail.com>
2022-05-17 13:20:49 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that creates a class 'Square' that defines a square"""
+
+
+class Square:
+    """created/defined the class 'Square'"""
+    pass
+    """empty block"""
