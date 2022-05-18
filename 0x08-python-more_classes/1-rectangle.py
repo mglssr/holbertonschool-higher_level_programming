037fc5086e215dbfe6106eeac1ab516f15bd3ba0
mglssr <iglesiasmaia6@gmail.com>
2022-05-18 16:04:51 -0700

    fix 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,16 +6,8 @@ class Rectangle:
     """creates the class"""
     def __init__(self, width=0, height=0):
         """initializes the class"""
-        self.__height = height
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
+        self.width = width
+        self.height = height
 
     @property
     def height(self):
