da64035f4ea26a116c05fee1a0a447f4d9ce5425
mglssr <iglesiasmaia6@gmail.com>
2022-05-23 14:28:33 -0700

    task 6 nd 8

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Task 7 Lazy matrix multiplication"""
+
+
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """function that multiplies 2 matrices by using the module NumPy"""
+    return (np.matmul(m_a, m_b))
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """unittest"""
+    def tests(self):
+        self.assertEqual(max_integer([1, 4, 10, 8]), 10)
+    def test1(self):
+        self.assertEqual(max_integer((1, 5, -50, 18)), 18)
+    def test3(self):
+        self.assertIsNotNone(max_integer([]), "Value is none")
+
+if __name__ == '__main__':
+    unittest.main()
