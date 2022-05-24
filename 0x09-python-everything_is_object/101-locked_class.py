95e84589976b5737412af2157e6f59af88a9ce1c
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-05-24 14:59:05 -0300

    task 31

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 class LockedClass:
     
-    def __init__(, first_name= " "):
-        self.first_name = first_name
-
-    @property
-    def first_name(self):
-        return self.first_name
+    __slots__ = 'first_name'
