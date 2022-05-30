04bc16fd53686b049b743617e94c00cf6a6f202a
mglssr <iglesiasmaia6@gmail.com>
2022-05-30 09:58:35 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""6. Improve Geometry"""
+
+
+class BaseGeometry:
+    """write a class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
