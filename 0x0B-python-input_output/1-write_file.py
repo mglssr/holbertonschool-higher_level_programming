b4f5908559a3ace0acaa42c227fa2ac8ac30e1fc
mglssr <iglesiasmaia6@gmail.com>
2022-05-31 16:26:34 -0700

    PEP8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """Write a function that writes a string to a text file\
-	and returns the number of characters written"""
+    and returns the number of characters written"""
     with open(filename, "w", encoding="utf-8") as _file:
         return (_file.write(text))
