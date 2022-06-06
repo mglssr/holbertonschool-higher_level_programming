098db89c592bc05b2c5e147f31d721fad9b454f3
mglssr <iglesiasmaia6@gmail.com>
2022-06-05 18:21:59 -0700

    tried task 16 :]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        import json
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        with open('cls' + '.json', "w", encoding="utf-8") as _file:
+            _file.write(Base.to_json_string(list_objs))
