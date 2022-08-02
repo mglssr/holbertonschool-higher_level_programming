abf474779b4efd6b7215102f3fffb79bf946dfe3
mglssr <iglesiasmaia6@gmail.com>
2022-08-02 08:35:09 -0700

    semi

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (elem of list) {
+  for (const elem of list) {
     if (elem === searchElement) {
       count++;
     }
