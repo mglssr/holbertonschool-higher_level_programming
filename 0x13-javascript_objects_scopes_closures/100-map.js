317c575adeac7722f7db1b03e00752ba256185f9
mglssr <iglesiasmaia6@gmail.com>
2022-08-02 10:23:13 -0700

    task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newlist = list.map(x => x * list.indexOf(x));
+const newlist = list.map((x, index) => x * index);
 console.log(list);
 console.log(newlist);
