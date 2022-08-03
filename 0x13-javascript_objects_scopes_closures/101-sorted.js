6993a373f525d733055eaa7c9802a0c5a684d2ed
mglssr <iglesiasmaia6@gmail.com>
2022-08-02 18:32:46 -0700

    task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-const newdict = {};
+const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]]) {
     newDict[dict[key]].push(key);
@@ -8,4 +8,4 @@ for (const key in dict) {
     newDict[dict[key]] = [key];
   }
 }
-console.log(newdict);
+console.log(newDict);
