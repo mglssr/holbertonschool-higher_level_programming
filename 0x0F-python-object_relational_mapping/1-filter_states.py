b4478259a977a6899129b6da26d9b4d97f966e03
mglssr <iglesiasmaia6@gmail.com>
2022-08-04 07:13:38 -0700

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa"""
+
+
+import MySQLdb
+import sys
+
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
+
+db = MySQLdb.connect(host="localhost", user=user,
+                     passwd=passwd, db=db)
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]'")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+cur.close()
+db.close()
