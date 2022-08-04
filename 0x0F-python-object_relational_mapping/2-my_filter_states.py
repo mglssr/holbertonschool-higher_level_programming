013e30d80c59b4c5917625c0f4b30c0eed5df668
mglssr <iglesiasmaia6@gmail.com>
2022-08-04 07:41:50 -0700

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa"""
+
+
+import MySQLdb
+import sys
+
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
+name = sys.argv[4]
+
+db = MySQLdb.connect(host="localhost", user=user,
+                     passwd=passwd, db=db)
+cur = db.cursor()
+query = ("SELECT * FROM states WHERE name = '%s'" % (name))
+cur.execute(query)
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+cur.close()
+db.close()
