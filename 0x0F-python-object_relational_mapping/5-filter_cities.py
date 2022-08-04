84c7a8489e1345f268d61f86e3919327e99f2f72
mglssr <iglesiasmaia6@gmail.com>
2022-08-04 12:32:41 -0700

    pycode

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,15 +9,20 @@ if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     db = sys.argv[3]
-    name = sys.argv[4]
+    state = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost", user=user,
                          passwd=passwd, db=db)
     cur = db.cursor()
-    query = ("SELECT * FROM states WHERE name = '%s'" % (name))
-    cur.execute(query)
+    query = "SELECT cities.name FROM cities WHERE state_id\
+            IN(SELECT id FROM states WHERE name = %s)"
+    cur.execute(query, (state,))
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    for row in rows[:-1]:
+        print(f"{row[0]}, ", end="")
+    if rows:
+        print(rows[-1][0])
+    else:
+        print()
     cur.close()
     db.close()
