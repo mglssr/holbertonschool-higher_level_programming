50f6d98ddeaa67d13f52ebbb0100eb06a1abee34
mglssr <iglesiasmaia6@gmail.com>
2022-08-19 17:23:39 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,10 @@ https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
-    from urllib import request
-    with request.urlopen('https://intranet.hbtn.io/status') as resp:
-        body = resp.read()
+    from urllib
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        body = response.read()
         print(f"Body response:")
-        print(f"    - type: {body.get[__class__]}")
-        print(f"    - content: {body}")
-        print(f"    - utf8 content: {body.decode('utf8')}")
+        print(f"\t- type: {}".format(type(body)))
+        print(f"\t- content: {}".format(body))
+        print(f"\t- utf8 content: {body.decode('utf8')}".format(body.decode('utf8')))
