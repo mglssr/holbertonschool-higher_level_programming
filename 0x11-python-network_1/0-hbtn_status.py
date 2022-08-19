4b4f0dfadabb3b1e1dd70a9a0e9e4dce8b6f9fce
mglssr <iglesiasmaia6@gmail.com>
2022-08-19 16:34:39 +0000

    permissions

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,10 @@ https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
-    import urllib
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        body = response.read()
+    from urllib import request
+    with request.urlopen('https://intranet.hbtn.io/status') as resp:
+        body = resp.read()
         print(f"Body response:")
-        print(f"    - type: {type(body)}")
+        print(f"    - type: {body.get[__class__]}")
         print(f"    - content: {body}")
         print(f"    - utf8 content: {body.decode('utf8')}")
