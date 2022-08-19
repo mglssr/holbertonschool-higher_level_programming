026cf56a3f7acdcd01630afa7b38dd2682849dcc
mglssr <iglesiasmaia6@gmail.com>
2022-08-19 22:33:15 +0000

    if, else rm

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,8 +10,4 @@ if __name__ == "__main__":
     passwrd = sys.argv[2]
     url = 'https://api.github.com/user'
     req = requests.get(url, auth=(user, passwrd))
-    q_json = req.json()
-    if q_json:
-        print("{}".format(q_json['id']))
-    else:
-        print("None")
+    print("{}".format(req.json().get('id')))
