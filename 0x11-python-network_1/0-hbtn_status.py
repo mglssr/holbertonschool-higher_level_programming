2a153243800db15501e0aefee55b9150ed2a9735
mglssr <iglesiasmaia6@gmail.com>
2022-08-19 13:27:41 +0000

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches
+https://intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    from urllib import request
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        body = response.read()
+        print(f"Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        print(f"    - utf8 content: {body.decode('utf8')}")
