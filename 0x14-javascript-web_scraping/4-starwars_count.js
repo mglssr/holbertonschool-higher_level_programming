4d0550e2bb06b71a7f4313bd82f013b98a420b82
mglssr <iglesiasmaia6@gmail.com>
2022-09-12 18:49:33 +0000

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const axios = require('axios').default;
+const argv = process.argv;
+const url = 'https://swapi-api.hbtn.io/api/people/18/';
+axios.get(argv[2])
+  .then(function (response) {
+    const results = response.data.results;
+    const ch = [];
+    results.forEach(element => ch.push(element.characters));
+    let i = 0;
+    let count = 0;
+    while (i < ch.length) {
+      let p = 0;
+      while (p < ch[i].length) {
+        if (ch[i][p] === url) {
+          count++;
+        }
+        p++;
+      }
+      i++;
+    }
+    console.log(count);
+  });
