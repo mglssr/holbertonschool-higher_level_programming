39394864cb1d4d0acb708f81a7f22ca0660d3a7d
mglssr <iglesiasmaia6@gmail.com>
2022-09-13 00:17:08 +0000

    semistandard fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,21 +3,21 @@ const argv = process.argv;
 const axios = require('axios').default;
 
 axios.get(argv[2])
-.then(function (response) {
-  const data = response.data;
-  const dict = {};
-  let i = 0
-  while (i < data.length) {
-    if (data[i].completed === true) {
-      if (dict[data[i].userId] !== undefined) {
-        dict[data[i].userId] = dict[data[i].userId] += 1;
-      } else {
-        dict[data[i].userId] = 1;
+  .then(function (response) {
+    const data = response.data;
+    const dict = {};
+    let i = 0;
+    while (i < data.length) {
+      if (data[i].completed === true) {
+        if (dict[data[i].userId] !== undefined) {
+          dict[data[i].userId] = dict[data[i].userId] += 1;
+        } else {
+          dict[data[i].userId] = 1;
+        }
       }
+      i++;
     }
-    i++;
-  }
-  console.log(dict);
-}).catch(function (error) {
-  console.log(`code: ${error.response.status}`);
-});
+    console.log(dict);
+  }).catch(function (error) {
+    console.log(`code: ${error.response.status}`);
+  });
